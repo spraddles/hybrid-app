e0586be6d42a603e32ba9ad6efad14f1c96f5822
Brett <badsprad@hotmail.com>
2020-01-03 23:19:36 +1100

    Updated cordova-prepare.sh

---
+++ cordova-prepare.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# remove Cordova temp dir:
+DIR_ROOT="./my_test_app"
+DIR="./$DIR_ROOT/www"
+if [ -d "$DIR" ]; then
+  # Take action if $DIR exists
+  rm -r $DIR
+  echo "Removed Cordova compiled files"
+fi
+
+# replace with Vue compiled files:
+cp -r ./dist/. $DIR
+
+# copy build.json to Cordova dir
+mv ./src/helpers/build.json $DIR_ROOT
+
+# echo
+echo Copied over build Vue compiled files
+echo Copied over build.json config for iOS builds
+echo Done...ready for Cordova build!!
