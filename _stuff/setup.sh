f0a36888e605e65fe926d8da234cfe4504b297b9
Brett <badsprad@hotmail.com>
2020-01-06 23:25:06 +1100

    Update setup.sh

--- _stuff/setup.sh
+++ _stuff/setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 
+#******************************
+#   INSTALL
+#******************************
+
 # OS dependencies
 npm
 cordova
@@ -9,7 +13,12 @@ iTSMTransporter
 
 
 # initials
+
+###### cmd > /dev/null #### hide output, show errors, show echo
+# show STDERR, hide STDOUT, show ECHO
+
 sudo -i
+yum update
 cd /
 cd root
 mkdir my_app
@@ -17,14 +26,44 @@ cd my_app
 
 
 # NPM
-yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
-sudo yum install -y nodejs
+yum install -y gcc-c++ make &> /dev/null
+curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash - &> /dev/null
+sudo yum install -y nodejs &> /dev/null
 echo 'NPM installed'
 
-# Cordova-cli
-npm install cordova -y
+
+# Git
+yum install git -y &> /dev/null
+git clone https://github.com/badsprad/testapp &> /dev/null
+echo 'Git installed'
+
+
+# Cordova
+npm install -g cordova &> /dev/null
+echo 'Cordova installed'
+
+
+
+#******************************
+#   CONFIG
+#******************************
+
+cordova telemetry off &> /dev/null
+cordova create my_test_app com.testapp.my &> /dev/null
+cd my_test_app
+cordova platform add ios &> /dev/null
+cordova platform add android &> /dev/null
+
+
 
 
 
 # App dependencies
+
+
+
+
+
+
+
+
