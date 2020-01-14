a1396f75f72a2ea93403e5e82cb9c8c803562da6
Brett <badsprad@hotmail.com>
2020-01-14 20:48:57 +1100

    Update theme-switch.sh

--- src/views/theme-switch.sh
+++ src/views/theme-switch.sh
@@ -6,15 +6,18 @@ LINE_MD=$(awk '/md/{print NR}' ./main.vue)
 LINE_IOS=$(awk '/ios/{print NR}' ./main.vue)
 
 # sh ./theme-switcher ios
-if [[ -n "$1" ]] && [[ $1 = "ios" ]] && [[ $LINE_MD = 3 ]]
+if [[ -n "$1" ]]
 then
-	# change to IOS
-	sed -i -e "3s/md/ios/" main.vue
-fi
-
-# sh ./theme-switcher md
-if [[ -n "$1" ]] && [[ $1 = "md" ]] && [[ $LINE_IOS = 3 ]]
-then
-	# change to MD
-	sed -i -e "3s/ios/md/" main.vue
+	# text = MD
+	if [[ $1 = "ios" ]] && [[ $LINE_MD = 3 ]]
+	then
+		# change to IOS
+		sed -i -e "3s/md/ios/" main.vue
+	fi
+	# text = IOS
+	if [[ $1 = "md" ]] && [[ $LINE_IOS = 3 ]]
+	then
+		# change to MD
+		sed -i -e "3s/ios/md/" main.vue
+	fi
 fi
