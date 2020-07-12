490f1dcad145fa85d172f8070bebfa3dd364b334
badsprad <4809356+badsprad@users.noreply.github.com>
2020-07-12 18:56:54 +1000

    File update

--- src/components/desktop-menu-side.vue
+++ src/components/desktop-menu-side.vue
@@ -5,10 +5,10 @@
         <b-list-group>
             <b-list-group-item class="list-group-item-logo active"><h3>[ logo here ]</h3></b-list-group-item>
             <b-list-group-item class="list-group-item-empty"></b-list-group-item>
-            <b-list-group-item to="/"><b-icon-house></b-icon-house>Home</b-list-group-item>
+            <b-list-group-item to="/home"><b-icon-house></b-icon-house>Home</b-list-group-item>
             <b-list-group-item to="/about"><b-icon-question-circle></b-icon-question-circle>About</b-list-group-item>
             <b-list-group-item to="/profile"><b-icon-person></b-icon-person>Profile</b-list-group-item>
-            <b-list-group-item to="/login"><b-icon-box-arrow-in-right></b-icon-box-arrow-in-right>Logout</b-list-group-item>
+            <b-list-group-item to="/#" @click="logout" class="list-group-item list-group-item-action"><b-icon-box-arrow-in-right ></b-icon-box-arrow-in-right>Logout</b-list-group-item>
         </b-list-group>
     </nav>
 
@@ -17,8 +17,37 @@
 
 <script>
 
+    import store from '../store.js';
+
     export default {
-        name: 'DesktopMenuSide'
+        
+        name: 'DesktopMenuSide',
+        
+        computed: {
+            isLoggedIn: function() {
+                return this.$store.getters.isLoggedIn
+            }
+        },
+        
+        methods: {
+            logout: function() {
+                this.$store.dispatch('logout')
+                    .then(() => {
+                        this.$router.push('/login')
+                    })
+            }
+        },
+        
+        created: function() {
+            this.$http.interceptors.response.use(undefined, function(err) {
+                return new Promise(function(resolve, reject) {
+                    if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+                        this.$store.dispatch(logout)
+                    }
+                    throw err;
+                });
+            });
+        }
     }
 
 </script>
